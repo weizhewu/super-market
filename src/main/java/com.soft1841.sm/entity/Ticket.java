af8a0ee9517a3c106fd2a2254ab7f6589bfe8cb8
YFuean <1412666584@qq.com>
2018-12-24 00:27:40 +0800

    做实体类

--- src/main/java/com.soft1841.sm/entity/Ticket.java
+++ src/main/java/com.soft1841.sm/entity/Ticket.java
@@ -1,4 +1,22 @@
 package com.soft1841.sm.entity;
 
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleLongProperty;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
 public class Ticket {
+    private final SimpleLongProperty ticketId = new SimpleLongProperty();
+    private final SimpleLongProperty cashierId = new SimpleLongProperty();
+    private final SimpleLongProperty vipId = new SimpleLongProperty();
+    private final SimpleDateFormat date = new SimpleDateFormat();
+    private final SimpleDoubleProperty count = new SimpleDoubleProperty();
+
+    public Ticket() {
+    }
+
+    public Ticket(long ticketId, long cashierId, long vipId, DateFormat date, double count){
+
+    }
 }
