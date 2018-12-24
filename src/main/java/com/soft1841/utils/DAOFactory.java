48f37d00569b2ec66d23e665d3625a8fee88a64d
232623 <https://github.com/weizhewu/->
2018-12-24 17:07:37 +0800

    DAOFactory完成

--- src/main/java/com/soft1841/utils/DAOFactory.java
+++ src/main/java/com/soft1841/utils/DAOFactory.java
@@ -1,12 +1,29 @@
 package com.soft1841.utils;
 
-import com.soft1841.dao.TypeDAO;
-import com.soft1841.dao.impl.TypeDAOImpl;
+import com.soft1841.dao.*;
+import com.soft1841.dao.impl.*;
+import com.soft1841.entity.Ticket;
 
 public class DAOFactory {
 
+    public static CashierDAO getCashierDAOInstance() {
+        return new CashierDAOImpl();
+    }
+    public static TicketDAO getTicketDAOInstance() {
+        return new TicketDAOImpl();
+    }
     public static TypeDAO getTypeDAOInstance() {
         return new TypeDAOImpl();
     }
+    public static VIPDAO getVIPDAOInstance() {
+        return new VIPDAOImpl();
+    }
+    public static DetailDAO getDetailDAOInstance() {
+        return new DetailDAOImpl();
+    }
+    public static GoodsDAO getGoodsDAOInstance() {
+        return new GoodsDAOImpl();
+    }
+
 
 }
